795a80aa7787c0662ae270164b32d330873b2a95
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-10-02 20:13:44 -0700

    chore: complete ex01.js

--- ex01.js
+++ ex01.js
@@ -1,9 +1,10 @@
 // Instructions: Use lodash's map function to double each number in the array.
 
-const _ = require('lodash');
+const _ = import("lodash");
 
 const numbers = [1, 2, 3, 4, 5];
 
 // your code here
+const doubled = _.map(numbers, (num) => num * 2);
 
 console.log(doubled); // Expected output: [2, 4, 6, 8, 10]
