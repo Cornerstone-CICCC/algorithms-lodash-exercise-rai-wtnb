811b85ec99213423785c454209c89e8d0a3bcbbf
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-10-02 20:14:03 -0700

    chore: complete ex03.js

--- ex03.js
+++ ex03.js
@@ -1,9 +1,10 @@
 // Instructions: Use lodash's find function to get the first number in the array that is greater than 5.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 3, 5, 7, 9];
 
 // your code here
+const firstGreaterThanFive = _.find(numbers, (num) => num > 5);
 
 console.log(firstGreaterThanFive); // Expected output: 7
